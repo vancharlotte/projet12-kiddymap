1873e6584aa088f6e1525e05b5b36517923eeb6b
charlotte <vanhuyse.charlotte@gmail.com>
2021-11-08 16:18:29 +0100

    first commit (api-gateway)

--- api-gateway/src/test/java/com/kiddymap/zuulserver/GatewayApplicationTests.java
+++ api-gateway/src/test/java/com/kiddymap/zuulserver/GatewayApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ZuulServerApplicationTests {
+class GatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
