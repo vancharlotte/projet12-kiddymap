353bf8ca7b35bce8f9071c74a073017c923f253e
charlotte <vanhuyse.charlotte@gmail.com>
2021-11-10 15:31:05 +0100

    add UUID id

--- microservice-location/src/main/java/com/kiddymap/microservicelocation/model/Location.java
+++ microservice-location/src/main/java/com/kiddymap/microservicelocation/model/Location.java
@@ -1,22 +1,27 @@
 package com.kiddymap.microservicelocation.model;
 
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.UUID;
 
 @Entity
 @Data
 public class Location {
 
     @Id
-    //@GeneratedValue(generator = "UUID")
-    //@GenericGenerator( name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @GeneratedValue(generator = "UUID2")
+    @GenericGenerator( name = "UUID2", strategy = "org.hibernate.id.UUIDGenerator")
     @Column(name = "id", updatable = false, nullable = false)
-    private String id;
+    private UUID id;
 
     private String name;
+    private String description;
+
 
 
 }
--- microservice-profil/src/main/java/com/kiddymap/microserviceprofil/model/Profil.java
+++ microservice-profil/src/main/java/com/kiddymap/microserviceprofil/model/Profil.java
@@ -1,33 +1,24 @@
 package com.kiddymap.microserviceprofil.model;
 
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Data
 public class Profil {
 
     @Id
-    //@GeneratedValue(generator = "UUID")
-    //@GenericGenerator( name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator( name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     @Column(name = "id", updatable = false, nullable = false)
-    private String id;
-
+    private UUID id;
 
     String username;
     String description;
-
-
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
