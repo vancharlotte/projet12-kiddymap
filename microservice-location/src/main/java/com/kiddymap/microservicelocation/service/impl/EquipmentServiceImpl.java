0dce136ea65f51f2afcea3228f4eb8eabcfb602a
charlotte <vanhuyse.charlotte@gmail.com>
2021-11-16 22:02:28 +0100

    add transactionnal

--- microservice-location/src/main/java/com/kiddymap/microservicelocation/service/impl/EquipmentServiceImpl.java
+++ microservice-location/src/main/java/com/kiddymap/microservicelocation/service/impl/EquipmentServiceImpl.java
@@ -6,11 +6,13 @@ import com.kiddymap.microservicelocation.model.Equipment;
 import com.kiddymap.microservicelocation.service.contrat.EquipmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@Transactional
 public class EquipmentServiceImpl implements EquipmentService {
 
     @Autowired
--- microservice-location/src/main/java/com/kiddymap/microservicelocation/service/impl/LocationServiceImpl.java
+++ microservice-location/src/main/java/com/kiddymap/microservicelocation/service/impl/LocationServiceImpl.java
@@ -5,11 +5,13 @@ import com.kiddymap.microservicelocation.model.Location;
 import com.kiddymap.microservicelocation.service.contrat.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@Transactional
 public class LocationServiceImpl implements LocationService {
 
     @Autowired
