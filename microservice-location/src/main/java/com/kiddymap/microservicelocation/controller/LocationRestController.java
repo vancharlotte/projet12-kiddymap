4615c519fbea5e34162ee78ed85a5a2b876e8730
charlotte <vanhuyse.charlotte@gmail.com>
2022-02-04 16:51:55 +0100

    madd properties to geoJSON

--- microservice-location/src/main/java/com/kiddymap/microservicelocation/controller/LocationRestController.java
+++ microservice-location/src/main/java/com/kiddymap/microservicelocation/controller/LocationRestController.java
@@ -25,6 +25,7 @@ public class LocationRestController {
 
     /**
      * Create - Add a new location
+     *
      * @param location An object location
      * @return The location object saved
      */
@@ -36,13 +37,14 @@ public class LocationRestController {
 
     /**
      * Read - Get one location
+     *
      * @param id The id of the location
      * @return A location object full filled
      */
     @GetMapping("/location/get/{id}")
     public LocationDTO getLocation(@PathVariable("id") final UUID id) {
         Optional<Location> location = locationService.getLocation(id);
-        if(location.isPresent()) {
+        if (location.isPresent()) {
             return modelMapper.map(location.get(), LocationDTO.class);
         } else {
             return null;
@@ -51,65 +53,79 @@ public class LocationRestController {
 
     /**
      * Read - Exist
+     *
      * @param latitude the lat of the location, long the long of the location
      * @return A location object full filled
      */
     @GetMapping("/location/exist/{lat}/{long}")
     public boolean existLocation(@PathVariable("lat") float latitude, @PathVariable("long") float longitude) {
-        return locationService.existLocation(latitude,longitude);
+        return locationService.existLocation(latitude, longitude);
 
     }
 
     /**
      * Read - Get all locations
+     *
      * @return - An Iterable object of location full filled
      */
     @GetMapping("/location/getAll")
     public Iterable<LocationDTO> getAllLocations() {
         Iterable<Location> locationList = locationService.getAllLocations();
-        return  modelMapper.map(locationList, new TypeToken<List<LocationDTO>>() {}.getType());
+        return modelMapper.map(locationList, new TypeToken<List<LocationDTO>>() {
+        }.getType());
     }
 
     @GetMapping("/location/getAllGeoJson/{minLat}/{maxLat}/{minLong}/{maxLong}")
-    public JSONObject getLocationsGeoJsonInBetween(@PathVariable("minLat") float minLat, @PathVariable("maxLat") float maxLat, @PathVariable("minLong")float minLong, @PathVariable("maxLong")float maxLong) {
+    public JSONObject getLocationsGeoJsonInBetween(@PathVariable("minLat") float minLat, @PathVariable("maxLat") float maxLat, @PathVariable("minLong") float minLong, @PathVariable("maxLong") float maxLong) {
         JSONObject featureCollection = new JSONObject();
         System.out.println("minLat : " + minLat + ", maxLat : " + maxLat);
         featureCollection.put("type", "featureCollection");
 
-            JSONArray featureList = new JSONArray();
+        JSONArray featureList = new JSONArray();
 
-           // for (Location obj : locationService.getAllLocations()) {
-         for (Location obj : locationService.getAllLocationsInBetween(minLat,maxLat,minLong,maxLong)) {
+        // for (Location obj : locationService.getAllLocations()) {
+        for (Location obj : locationService.getAllLocationsInBetween(minLat, maxLat, minLong, maxLong)) {
 
-                JSONObject point = new JSONObject();
+            JSONObject feature = new JSONObject();
 
-                point.put("type", "Point");
+            JSONObject point = new JSONObject();
 
-                List<Float> coordinates = new ArrayList<>();
-                coordinates.add(obj.getLongitude());
-                coordinates.add(obj.getLatitude());
+            List<Float> coordinates = new ArrayList<>();
+            coordinates.add(obj.getLongitude());
+            coordinates.add(obj.getLatitude());
 
-                point.put("coordinates", coordinates);
-                JSONObject feature = new JSONObject();
-                feature.put("geometry", point);
-                feature.put("type", "Feature");
-                featureList.add(feature);
+            point.put("type", "Point");
+            point.put("coordinates", coordinates);
 
-            }
+            JSONObject properties = new JSONObject();
+            properties.put("name", obj.getName());
+            properties.put("description", obj.getDescription());
+         //   properties.put("equipment", obj.getEquipments());
+
+            feature.put("geometry", point);
+            feature.put("type", "Feature");
+            feature.put("properties", properties);
+
+            featureList.add(feature);
+
+
+        }
+
         featureCollection.put("features", featureList);
         System.out.println(featureCollection.toString());
 
-        return  featureCollection;
+        return featureCollection;
     }
 
     /**
      * Update - Update an existing location
-     * @param id - The id of the location to update
+     *
+     * @param id       - The id of the location to update
      * @param location - The location object updated
      * @return
      */
     @PutMapping("/location/update/{id}")
-    public Location updateLocation (@PathVariable("id") final UUID id, @RequestBody Location location) {
+    public Location updateLocation(@PathVariable("id") final UUID id, @RequestBody Location location) {
         Optional<Location> e = locationService.getLocation(id);
         if (e.isPresent()) {
             Location currentLocation = e.get();
@@ -129,6 +145,7 @@ public class LocationRestController {
 
     /**
      * Delete - Delete a location
+     *
      * @param id - The id of the location to delete
      */
     @DeleteMapping("/location/delete/{id}")
